9639080c32e49b1403e440dd3399074dfcecb729
kushwahapreeti8882-sys <kushwahapreeti8882@gmail.com>
2026-09-08 13:15:42 +0530

    Update bot token and admin user IDs

--- new.py
+++ new.py
@@ -66,10 +66,10 @@ import os
 # Join :- @DX4_CHEATS #from keep_alive import keep_alive
 # Join :- @DX4_CHEATS #keep_alive()
 # Join :- @DX4_CHEATS # insert your Telegram bot token here
-bot = telebot.TeleBot('7141965785:AAFRsCyY6y_iW4r_GMxkXdRclUGBVRF0fYc')
+bot = telebot.TeleBot('8816207241:AAH7G9LhIqhqyr5WNiXp_lhCBqxjTB1tRCA')
 
 # Join :- @DX4_CHEATS # Admin user IDs
-admin_id = ["1470983554","1908095816","1537783826"]
+admin_id = ["6578599193","6578599193","6578599193"]
 
 # Join :- @DX4_CHEATS # File to store allowed user IDs
 USER_FILE = "users.txt"
